ea9da9ec879da8804855e20d19de114800a6747f
Erwin Suarez <t00189596@365s.ittralee.ie>
2016-10-19 15:37:43 +0100

    Original

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)HelloWorld.java
+ *
+ *
+ * @Erwin Suarez
+ * @version 1.00 2016/10/19
+ */
+
+
+public class HelloWorld {
+
+    public HelloWorld() {
+    	System.out.print("Hello world!");
+    }    
+}
